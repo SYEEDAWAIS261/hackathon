1500147e69d284f69bb9b4dc69c8b65576b9102b
SYEEDAWAIS261 <awaisshah14101@icloud.com>
2025-06-21 10:28:35 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hacathon2/', // 👈 Add this line (must match your GitHub repo name)
   build: {
     rollupOptions: {
-      input: 'index.html',
-    },
-  },
+      input: 'index.html', // Ensure that this points to the correct location
+    }
+  }
 });
