c3585a48646e94eb4df249c9c93c6d4e851efe74
SYEEDAWAIS261 <awaisshah14101@icloud.com>
2025-06-21 09:16:56 -0700

    add last commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflex-reactclone/', // 👈 Add this line (must match your GitHub repo name)
+  base: '/hacathon2/', // 👈 Add this line (must match your GitHub repo name)
   build: {
     rollupOptions: {
       input: 'index.html',
